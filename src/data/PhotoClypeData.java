2a6d0b1af0791477184e6450085f41d71f3e0906
carterce1997 <carterce1997@gmail.com>
2017-12-05 23:26:30 -0500

    added PhotoClypeData

---
+++ src/data/PhotoClypeData.java
@@ -0,0 +1,27 @@
+package data;
+
+public class PhotoClypeData extends ClypeData {
+
+	public PhotoClypeData(String userName, int type) {
+		super(userName, type);
+	}
+
+	public PhotoClypeData(int type) {
+		super(type);
+	}
+
+	public PhotoClypeData() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String getData() {
+		return null;
+	}
+
+	@Override
+	public String getData(String key) {
+		return null;
+	}
+
+}
